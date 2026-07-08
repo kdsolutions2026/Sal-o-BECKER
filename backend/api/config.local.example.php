63bd37426ca8bf937976d7fc562dfd876a13e912
kdsolutions2026 <comercial.kdsolutions@gmail.com>
2026-07-08 17:23:06 -0300

    Delete backend/api directory

--- backend/api/config.local.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'public_key' => 'TEST-xxxx',
-    'access_token' => 'TEST-xxxx',
-    'webhook_url' => 'https://seu-dominio.com/api/webhook-mercadopago.php',
-];
--- backend/api/config.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'public_key' => 'APP_USR-3f2c2952-4e84-4830-8d34-78b1aa69b600',
-    'access_token' => 'APP_USR-8285019164679177-070813-13600d4095f8bea9998ac56252332959-3526815931',
-    'webhook_url' => 'https://seu-dominio.com/api/webhook-mercadopago.php',
-];
