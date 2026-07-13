6acaafd3edfa128f6d000b3c3d73d6689dfe62b4
kdsolutions2026 <comercial.kdsolutions@gmail.com>
2026-07-13 10:32:48 -0300

    Delete scripts directory

--- scripts/runtime-config.example.js
+++
@@ -1,7 +0,0 @@
-window.BECKER_RUNTIME = {
-  // Exemplo:
-  // apiBaseUrl: "https://bobpylnarpvhhicwrnma.supabase.co/functions/v1",
-  // apiKey: "SUA_PUBLISHABLE_KEY_DO_SUPABASE",
-  apiBaseUrl: "",
-  apiKey: "",
-};
--- scripts/runtime-config.js
+++
@@ -1,5 +0,0 @@
-window.BECKER_RUNTIME = window.BECKER_RUNTIME || {
-  // Projeto Supabase Sal-o-BECKER
-  apiBaseUrl: "https://bobpylnarpvhhicwrnma.supabase.co/functions/v1",
-  apiKey: "sb_publishable_BSRGFfS2gfms8-VzA4c-zw_yvdPFgf2",
-};
